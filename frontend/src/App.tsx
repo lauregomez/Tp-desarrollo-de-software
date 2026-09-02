64023206bff7f8ce3fdb658912a1b1a3ae4749cc
lauregomez <lauribgomez@gmail.com>
2026-09-01 23:29:31 -0300

    feat: add match card and matches listing with mock data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
 import MainLayout from './components/layout/MainLayout'
 import AuthLayout from './components/layout/AuthLayout'
-import PartidosPage from './pages/PartidosPage'
+import MatchesPage from './pages/MatchesPage'
 import LoginPage from './pages/LoginPage'
 
 export default function App() {
   return (
     <Routes>
       <Route element={<MainLayout />}>
-        <Route path="/" element={<PartidosPage />} />
+        <Route path="/" element={<MatchesPage />} />
       </Route>
 
       <Route element={<AuthLayout />}>
